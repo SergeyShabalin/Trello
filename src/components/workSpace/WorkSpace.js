dfd5e8ac5934c62e35ead3633e6703d687f3bd2c
ssworkss <obaldevshiy@yandex.ru>
2022-08-31 11:29:52 +0300

    structure folders list wrapper

--- src/components/workSpace/WorkSpace.js
+++ src/components/workSpace/WorkSpace.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Board from "./board/Board";
 
 export default function WorkSpace(){
 
     return(
-        <div>Рабочее пространство</div>
+        <div>
+            Рабочее пространство
+            <div><Board/></div>
+        </div>
     )
 }
---
+++ src/components/workSpace/board/Board.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ListWrapper from "./listWrapper/ListWrapper";
+
+export default function Board(){
+    return (
+        <div>
+           Board
+            <ListWrapper/>
+            <ListWrapper/>
+            <ListWrapper/>
+            <ListWrapper/>
+        </div>
+    );
+};
+
