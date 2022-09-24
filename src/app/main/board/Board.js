90c3dcfe3a0a53222dd8170a29e307634a194282
ssworkss <obaldevshiy@yandex.ru>
2022-09-24 22:02:24 +0300

    background and server

--- src/app/main/board/Board.js
+++ src/app/main/board/Board.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ListWrapper from "./listWrapper/ListWrapper";
-import classes from './styles/Board.module.css'
 import Button from "../../../components/basic/button/Button";
 import {AiOutlinePlus} from "react-icons/ai";
+import classes from './styles/Board.module.css'
 
 export default function Board() {
     return (
         <div className={classes.board}>
 
             <div className={classes.wrapper_list}>
-                <ListWrapper header={'Предстоит сделать'}/>
-                <ListWrapper header={'В работе'}/>
-                <ListWrapper header={'Готово'}/>
+                <ListWrapper header='Предстоит сделать'/>
+                <ListWrapper header='В работе'/>
+                <ListWrapper header='Готово'/>
             </div>
             <div className={classes.add_list}>
                 <Button
@@ -24,4 +24,6 @@ export default function Board() {
         </div>
     );
 };
+
+
 
--- src/app/main/board/listWrapper/ListWrapper.js
+++ src/app/main/board/listWrapper/ListWrapper.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ListHeader from "./listHeader/ListHeader";
 import ListCard from "./listCard/ListCard";
 import CardCreator from "./cardCreator/CardCreator";
@@ -15,4 +16,8 @@ export default function ListWrapper({header}) {
         </div>
     );
 };
+
+ListWrapper.propTypes ={
+    header: PropTypes.string
+}
 
